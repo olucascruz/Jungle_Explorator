95d3cecdad0dda28ef9774b9ef6ad2ab1c4a2c08
LucasCruz <lca.snf21@uea.edu.br>
2022-05-12 23:26:31 -0400

    Add Player and CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+   public Vector3 offset;
+
+   private void FixedUpdate()
+   {
+       transform.position = target.position + offset;
+   }
+}
