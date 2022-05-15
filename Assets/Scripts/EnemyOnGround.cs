e11d058ea522a23810146726183001e511d6e00c
LucasCruz <lca.snf21@uea.edu.br>
2022-05-15 08:02:25 -0400

    update collision EnemyOnGround

--- Assets/Scripts/EnemyOnGround.cs
+++ Assets/Scripts/EnemyOnGround.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class EnemyOnGround : MonoBehaviour
 {
-   public float speed;
-   public float distance;
+    private Rigidbody2D rig;
+    public float speed;
 
-   bool isRight = true;
+    public Transform headPoint;
+    public Transform rightCol;
+    public Transform leftCol;
+    public bool colliding;
+    
+    public float distance;
 
-   public Transform groundCheck;
+    bool isRight = true;
 
+    public Transform groundCheck;
+
+    public LayerMask layer;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
         RaycastHit2D ground = Physics2D.Raycast(groundCheck.position, Vector2.down, distance);
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
 
-        if(ground.collider == false)
+        if(ground.collider == false || colliding)
         {
             if(isRight)
             {
@@ -35,9 +50,14 @@ public class EnemyOnGround : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        if(collision.gameObject.tag == "Player")
         {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
 
+            if(height > 0){
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                Destroy(gameObject, 0.5f);
+            }
         }
     }
 }
