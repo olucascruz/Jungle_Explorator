913978acff2b05351f650cd202e5f2bd4c50be78
Lidia Dias de Souza <ldds.snf21@uea.edu.br>
2022-05-16 00:58:08 -0300

    creating main menu

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControler : MonoBehaviour
+{
+    public void RestartGame(string lvlName )
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+}
