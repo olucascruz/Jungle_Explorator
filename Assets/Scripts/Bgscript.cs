df86f48c5d693c240db2f0483d982e96d0662b39
Adriano-Henrique-de-Souza-Andrade <ahdsa.snf21@uea.edu.br>
2022-05-16 14:57:41 -0400

    Fix Music
    -now it play in every stage

---
+++ Assets/Scripts/Bgscript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgscript : MonoBehaviour
+{
+    public static Bgscript BgInstance;
+
+    private void Awake()
+    {
+        if(BgInstance != null && BgInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        BgInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}
